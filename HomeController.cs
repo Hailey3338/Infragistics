6b712872215b9c4ad05e8fe72bf1a2cea52b97d2
Hailey3338 <Hailey3338@hotmail.com>
2018-11-06 22:09:41 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grid.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserEntities _context = new UserEntities();
+        // GET: Home
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetUser()
+        {
+            return Json(this._context.User.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ UserGrid.js
@@ -0,0 +1,34 @@
+﻿
+$(function () {
+    var data = $("#user");
+    var uSourceUrl = data.attr("data-user");
+    $("#user").igGrid({
+        columns: [
+            {
+                headerText: "Id",
+                key: "Id",
+                dataType: "number",
+                hidden: true
+            },
+            {
+                headerText: "User Name",
+                key: "UserName",
+                dataType: "string"
+            },
+            {
+                headerText: "Email",
+                key: "Email",
+                dataType: "string"
+
+            },
+            {
+                headerText: "Added Date",
+                key: "AddedDate",
+                dataType: "any"
+            }
+        ],
+        width: '100%',
+        dataSource: uSourceUrl
+    });
+});
+    
